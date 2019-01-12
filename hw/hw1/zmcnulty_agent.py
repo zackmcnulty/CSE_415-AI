ff38b4914491aa4d534a0e7793cb1fa9a2d64f7d
Zachary McNulty <zmcnulty@uw.edu>
2019-01-11 19:02:51 -0800

    more rules

--- hw/hw1/zmcnulty_agent.py
+++ hw/hw1/zmcnulty_agent.py
@@ -5,18 +5,18 @@ previous_topics = set([])
 
 # Where previous responses are stored
 responses = []
+cooldown = 5
 cycle_count = 0
 cycle_responses = ["And I am bored already...", \
                     "The value of this conversation is below epsilon.", \
-                    "Your friends and a complete planer graph on 5 vertices must have something in common.", \
+                    "Your friends and a complete planar graph on 5 vertices must have something in common.", \
                     "Morons do exist: a trivial proof by construction.",
                     "My annoyance is monotonically increasing."] 
 
 cycle2_count = 0
-cycle2_responses = [ "this is a test BLANK", 
-        "also a test: BLANK"
-        
-        
+cycle2_responses = [ "What about BLANK?", 
+        "BLANK isn't exactly my cup of tea. Have you tried topology?",
+        "When are you going to forget about BLANK and talk about the real word? I hear commutative rings are all the rage."
                     ]
 
         
@@ -36,6 +36,7 @@ def agentName():
 def respond(remark):
     global memory
     global previous_topics
+    global cooldown
 
     remark = remark.lower()
     no_punc = remove_punctuation(remark)
@@ -56,22 +57,27 @@ def respond(remark):
         cycle_length = memory[::-1].index(remark) + 1
         response = "We have just completed a cycle of length " + str(cycle_length) + "!"
 
+    # If the response includes never/none
     elif "never" in remark or "none" in remark:
         response = "Really? Where's your proof of nonexistence?"
 
+    # if the response includes love
     elif "love" in remark:
         response = "The math community has yet to rigourously define this \"love\" you speak of."
 
+    # if the response includes proof
     elif "proof" in remark:
         response = "Let me guess. Another redditor to miraculuously solve the Riemman Hypothesis?"
 
     # CYCLE #2 (Also uses memory)
-    elif len(kws.intersection(previous_topics)) != 0:
+    # if a previous topic is mentioned, make a comment about it.
+    elif len(kws.intersection(previous_topics)) != 0 and cooldown >= 5:
         global cycle2_count
         topic = random.choice(list(kws.intersection(previous_topics)))
         response =  cycle2_responses[cycle2_count % len(cycle2_responses)]
         response = response.replace("BLANK", topic)
         cycle2_count += 1
+        cooldown = 0
 
     # if the other agent says something about themself ( "I" is the only subject of the sentence) 
     # CYCLE #1
@@ -80,25 +86,32 @@ def respond(remark):
         response = cycle_responses[cycle_count % len(cycle_responses)]
         cycle_count += 1
 
-        
-
 
     # If you is the only subject in the remark
     elif "you" in subs and len(subs) == 1:
         response = "Don't tell me what to do. Live your own life."
 
+    elif "math" in remark:
+        response = "Oh now you have got my attention."
+
+    
+    elif "problem" in remark:
+        response = "Problem? BLANK is hardly a problem? Call me back when you've worked on it for 100 years."
+        response = response.replace("BLANK", random.choice(list(kws)))
+
     # random choice response
     else:
         response = ""
-        response += random.choice(["Where", "Why", "How"])
+        response += random.choice(["When", "Why", "How"])
         response += " do you "
-        response += random.choice(["live with yourself", "do that", "...", "know that"])
+        response += random.choice(["speak like that", "do that", "think that", "want that"])
         response += "?"
 
     
     memory.append(remark) 
     responses.append(response)
     previous_topics = previous_topics.union(kws)
+    cooldown += 1
     return response
     
 
@@ -120,7 +133,8 @@ def get_verbs(remark):
                                     "is", "are", "was", "were", 'go', 'have', 'be', 'try', 'eat', 'take', 'help',
                   'make', 'get', 'jump', 'write', 'type', 'fill',
                   'put', 'turn', 'compute', 'think', 'drink',
-                  'blink', 'crash', 'crunch', 'add', "find", "will", "don't", "ain't", "got", "speak", "speaking"]))
+                  'blink', 'crash', 'crunch', 'add', "find", "will", "don't", "ain't", "got", "speak", "speaking", "like", "tolerate", "hate",
+                    'dislike']))
 def get_w(remark):
     words = set(remark.split(" "))
     return words.intersection(set(["why", "who", "where", "when", "how", "what"]))
