21d5980df5cd9494367999ba026477709a499ca1
Zachary McNulty <zmcnulty@uw.edu>
2019-02-06 00:02:32 -0800

    feed forward for static evals

--- hw/hw4/a4_starter_code/zmcnulty_TTS_agent.py
+++ hw/hw4/a4_starter_code/zmcnulty_TTS_agent.py
@@ -97,6 +97,23 @@ def take_turn(current_state, last_utterance, time_limit):
             else: WHITE.append(spot)
 
     # Look for a better move using some actual strategy
+    DEPTHS = {current_state:0} # NOTE: have these persist from previous runs????
+    BACKLINKS = {}
+    max_depth = -1
+
+    #NOTE: assume max_ply is < actually max depth of the system?
+    while max_depth < max_ply and time.time() - start_time < time_limit:
+
+      # results = [current_state static eval, n_states expanded, n static evals, n ab cutoffs]
+      # returns None if ran out of time
+      max_depth += 1
+
+      # begin search at last layer previously explored.
+      initial_open = [state for state in DEPTHS if DEPTHS[state] == max_depth - 1]
+
+      # have this return best move...
+      dfs_results = DFS(initial_open, DEPTHS, current_state, max_depth, use_default_move_ordering, alpha_beta, time_limit - (time.time() - start_time))
+    
 
     # make the move we have decided on
     new_state.board[move[0]][move[1]] = who
@@ -182,6 +199,9 @@ def parameterized_minimax(
 
   # MY CODE!
 
+  # NOTE: how do we decide whose turn it is???
+  who = 'B'
+
   # use my custom static eval function if told so
   global USE_CUSTOM_STATIC_EVAL_FUNCTION
   USE_CUSTOM_STATIC_EVAL_FUNCTION = use_custom_static_eval_function
@@ -189,16 +209,29 @@ def parameterized_minimax(
   # use my custom TTS state object
   current_state.__class__ = My_TTS_State
 
+  # will this interfere with the order in which I traverse states over?
+  # i.e. cannot adhere to use_default_move_ordering
+  DEPTHS = {current_state:0}
+  BACKLINKS = {}
+
   if use_iterative_deepening_and_time:
       max_depth = -1
 
-      #NOTE: assume max_play is < actually max depth of the system?
+      #NOTE: assume max_ply is < actually max depth of the system?
       while max_depth < max_ply and time.time() - start_time < time_limit:
 
           # results = [current_state static eval, n_states expanded, n static evals, n ab cutoffs]
           # returns None if ran out of time
           max_depth += 1
-          dfs_results = DFS(current_state, max_depth, use_default_move_ordering, alpha_beta, time_limit - (time.time() - start_time))
+
+          # begin search at last layer previously explored.
+          # save this for above; initial_open = [state for state in DEPTHS if DEPTHS[state] == max_depth - 1]
+
+          # UNLIKE our hw2, if we find a state that is 5 moves away, there is no
+          # way for us to reach that state in less than 5 moves so we do not have to worry about
+          # rexpanding given states (save that for later
+          initial_open = [current_state]
+          dfs_results = DFS(initial_open, DEPTHS, current_state, max_depth, use_default_move_ordering, alpha_beta, time_limit - (time.time() - start_time))
         
           if results != None:
               current_state_static_val = dfs_results[0]
@@ -210,7 +243,8 @@ def parameterized_minimax(
   else: 
       # just run DFS with the max depth starting at the max_play, and an unreasonably high time limt
       # so that its not an issue.
-      results = DFS(current_state, max_ply, use_default_move_ordering, alpha_beta, 10**10)
+      initial_open = [current_state]
+      results = DFS(who, initial_open, DEPTHS, current_state, max_ply, use_default_move_ordering, alpha_beta, 10**10)
 
       current_state_static_val = dfs_results[0]
       n_states_expanded += dfs_results[1]
@@ -229,5 +263,46 @@ def parameterized_minimax(
   # Actually return the list of all results...
   return(results)
 
-def DFS(current_state, max_depth, use_default_move_ordering, alpha_beta, time_limit):
-    OPEN = [current_state]
+# NOTE: See HW2 IDDFS.py for an example of how this was implemented; I reused most of my ideas from there
+def DFS(who, initial_open, DEPTHS, current_state, max_depth, use_default_move_ordering, alpha_beta, time_limit):
+   start_time = time.time()
+
+   states_expanded = 0
+   static_evals = 0
+   num_ab_cutoffs = 0
+
+   OPEN = initial_open 
+
+   while OPEN != []:
+       #NOTE: might want to give a few fractions of a second in leeway
+       if time.time() - start_time > time_limit: 
+           return None
+
+       S = OPEN.pop(0)
+      
+       if DEPTHS[S] < max_depth:
+
+           states_expanded += 1
+
+           L = []
+           for i, row in enumerate(S.board):
+               for j, tile in enumerate(row):
+                   if tile == ' ':
+                       new_state = My_TTS_State(S.board)
+                       new_state.board[i][j] = who
+                       DEPTHS[new_state] = DEPTHS[S] + 1
+                       L.append(new_state)         
+
+           OPEN = L + [o for o in OPEN if o not in L]
+        else:
+            # calculate its static evaluation
+            # have set of FORWARD LINKS so we can percorlate the 
+            # static evaluation scores up using minimax?
+            static_eval += 1
+
+        
+        if who == 'B': who = 'W'
+        else: who = 'B'
+
+
+   return [current_state_static_val, states_expanded, static_evals, num_ab_cutoffs]
